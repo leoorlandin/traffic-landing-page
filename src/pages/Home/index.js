5799bf7d1adf98055e0e904a3788416c04350bee
unknown <leonardo.1117@hotmail.com>
2020-07-31 21:01:02 -0300

    fix svg source

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-// import * as S from './styles'
+import * as S from './styles';
 
-import PrimaryButton from '../../components/PrimaryButton'
+import Scene from '../../assets/Scene.svg';
+
+import PrimaryButton from '../../components/PrimaryButton';
+
 
 const Home = () => {
   return (
     <>
-      <PrimaryButton height={40} width={100} textContent="WORKS"/>
+      <S.MainBanner>
+        <img src={Scene} alt="background banner" />
+        <S.BannerContent>
+          <h1>Elo</h1>
+          <PrimaryButton width={220} height={60} textContent="GET STARTED"></PrimaryButton>
+        </S.BannerContent>
+      </S.MainBanner>
     </>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components';
 
-export const test = styled.div`
-  background-color: red;
-  color: white;
+export const MainBanner = styled.div`
+  min-height: auto;
+
+  & img {
+  position: sticky;
+  }
+`;
+
+
+export const BannerContent = styled.div`
+
+  min-height: auto;
+  display: flex;
+  position: absolute;
+
+h1 {
+  color: #fff;
+  font-size: 60px;
+}
+
+button {
+  font-size: 16px;
+}
+
+
 `;
